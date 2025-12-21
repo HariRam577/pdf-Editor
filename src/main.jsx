5552eef6c405063dd663905d932343e3e3076c5f
HariRam577 <hariram007cm@gmail.com>
2025-12-21 23:54:18 +0530

    android changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../public/service-worker.js")
+      .then(() => console.log("SW registered"))
+      .catch((err) => console.log("SW failed", err));
+  });
+}
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
